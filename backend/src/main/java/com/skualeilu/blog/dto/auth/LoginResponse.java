2e951c849c6de713ad3c02d97d9ae9fcb54860b9
MakinoharaShoko <148352813+Makinohharashokosan@users.noreply.github.com>
2026-01-16 00:25:03 +0800

    Delete backend directory
    Delete backend directory

--- backend/src/main/java/com/skualeilu/blog/dto/auth/LoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.skualeilu.blog.dto.auth;
-
-public class LoginResponse {
-    private String token;
-    public LoginResponse(String token) { this.token = token; }
-    // getter and setter
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
-}
