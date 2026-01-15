5a85e02bbca30a361b9e87388688659a322a35fa
MakinoharaShoko <148352813+Makinohharashokosan@users.noreply.github.com>
2026-01-16 00:34:36 +0800

    Delete backend directory
    Delete backend directory

--- backend/src/main/java/com/skualeilu/blog/dto/request/UpdateTagRequest.java
+++
@@ -1,8 +0,0 @@
-package com.skualeilu.blog.dto.request;
-import jakarta.validation.constraints.NotEmpty;
-public class UpdateTagRequest {
-    @NotEmpty
-    private String name;
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-}
--- backend/src/main/java/com/skualeilu/blog/repository/SocialLinkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skualeilu.blog.repository;
-
-import com.skualeilu.blog.entity.SocialLink;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SocialLinkRepository extends JpaRepository<SocialLink, Long> {
-}
